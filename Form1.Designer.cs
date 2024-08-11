1840920922878fc0a1ec771d37f6d66df2190edc
p341209 <p341209@tafe.wa.edu.au>
2024-08-11 14:08:45 +0800

    Main form for Astronomical Processing program
    Main form for Astronomical Processing program

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,26 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
+            lbl_Page_Heading = new Label();
+            txt_Search_dataList = new TextBox();
+            btn_Write_File = new Button();
+            lbl_Search_dataList = new Label();
+            lbl_Write_File = new Label();
+            btn_Search_dataFile = new Button();
+            lbl_show_list = new Label();
+            btn_show_data = new Button();
             SuspendLayout();
             // 
-            // label1
+            // lbl_Page_Heading
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(198, 24);
-            label1.Name = "label1";
-            label1.Size = new Size(138, 15);
-            label1.TabIndex = 0;
-            label1.Text = "Astronomical Processing";
-            label1.Click += label1_Click;
+            lbl_Page_Heading.AutoSize = true;
+            lbl_Page_Heading.Location = new Point(252, 27);
+            lbl_Page_Heading.Name = "lbl_Page_Heading";
+            lbl_Page_Heading.Size = new Size(138, 15);
+            lbl_Page_Heading.TabIndex = 0;
+            lbl_Page_Heading.Text = "Astronomical Processing";
+            lbl_Page_Heading.Click += lbl_Page_Heading_Click;
+            // 
+            // txt_Search_dataList
+            // 
+            txt_Search_dataList.Location = new Point(272, 78);
+            txt_Search_dataList.Name = "txt_Search_dataList";
+            txt_Search_dataList.Size = new Size(100, 23);
+            txt_Search_dataList.TabIndex = 1;
+            txt_Search_dataList.TextChanged += txt_Search_dataList_TextChanged;
+            // 
+            // btn_Write_File
+            // 
+            btn_Write_File.Location = new Point(272, 130);
+            btn_Write_File.Name = "btn_Write_File";
+            btn_Write_File.Size = new Size(100, 23);
+            btn_Write_File.TabIndex = 2;
+            btn_Write_File.Text = "Write File";
+            btn_Write_File.UseVisualStyleBackColor = true;
+            btn_Write_File.Click += btn_Write_File_Click;
+            // 
+            // lbl_Search_dataList
+            // 
+            lbl_Search_dataList.AutoSize = true;
+            lbl_Search_dataList.Location = new Point(104, 81);
+            lbl_Search_dataList.Name = "lbl_Search_dataList";
+            lbl_Search_dataList.Size = new Size(162, 15);
+            lbl_Search_dataList.TabIndex = 3;
+            lbl_Search_dataList.Text = "Enter an integer to search for:";
+            lbl_Search_dataList.Click += lbl_Search_dataList_Click;
+            // 
+            // lbl_Write_File
+            // 
+            lbl_Write_File.AutoSize = true;
+            lbl_Write_File.Location = new Point(117, 138);
+            lbl_Write_File.Name = "lbl_Write_File";
+            lbl_Write_File.Size = new Size(149, 15);
+            lbl_Write_File.TabIndex = 4;
+            lbl_Write_File.Text = "Write the data to a text file:";
+            lbl_Write_File.Click += lbl_Write_File_Click;
+            // 
+            // btn_Search_dataFile
+            // 
+            btn_Search_dataFile.Location = new Point(402, 78);
+            btn_Search_dataFile.Name = "btn_Search_dataFile";
+            btn_Search_dataFile.Size = new Size(75, 23);
+            btn_Search_dataFile.TabIndex = 5;
+            btn_Search_dataFile.Text = "Search";
+            btn_Search_dataFile.UseVisualStyleBackColor = true;
+            btn_Search_dataFile.Click += btn_Search_dataFile_Click;
+            // 
+            // lbl_show_list
+            // 
+            lbl_show_list.AutoSize = true;
+            lbl_show_list.Location = new Point(186, 188);
+            lbl_show_list.Name = "lbl_show_list";
+            lbl_show_list.Size = new Size(80, 15);
+            lbl_show_list.TabIndex = 6;
+            lbl_show_list.Text = "Show all data:";
+            // 
+            // btn_show_data
+            // 
+            btn_show_data.Location = new Point(272, 180);
+            btn_show_data.Name = "btn_show_data";
+            btn_show_data.Size = new Size(100, 23);
+            btn_show_data.TabIndex = 7;
+            btn_show_data.Text = "Show Data";
+            btn_show_data.UseVisualStyleBackColor = true;
+            btn_show_data.Click += btn_show_data_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(554, 254);
-            Controls.Add(label1);
+            BackColor = SystemColors.Window;
+            ClientSize = new Size(620, 359);
+            Controls.Add(btn_show_data);
+            Controls.Add(lbl_show_list);
+            Controls.Add(btn_Search_dataFile);
+            Controls.Add(lbl_Write_File);
+            Controls.Add(lbl_Search_dataList);
+            Controls.Add(btn_Write_File);
+            Controls.Add(txt_Search_dataList);
+            Controls.Add(lbl_Page_Heading);
             Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Astronomical Processing";
             ResumeLayout(false);
             PerformLayout();
@@ -55,6 +138,13 @@
 
         #endregion
 
-        private Label label1;
+        private Label lbl_Page_Heading;
+        private TextBox txt_Search_dataList;
+        private Button btn_Write_File;
+        private Label lbl_Search_dataList;
+        private Label lbl_Write_File;
+        private Button btn_Search_dataFile;
+        private Label lbl_show_list;
+        private Button btn_show_data;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,15 +1,78 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
 namespace Astronomical_Processing
 {
     public partial class Form1 : Form
+
     {
+        // Create an integer list with 24 elements, available to all functions
+        List<int> lst_astro_data = new List<int>() { 10, 20, 30, 40, 11, 21, 31, 41, 12, 22, 32, 42, 13, 23, 33, 43, 14, 24, 34, 44, 15, 25, 35, 45, };
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void lbl_Page_Heading_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void txt_Search_dataList_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Write_File_Click(object sender, EventArgs e)
+        {
+            // write the contents of the dataList to a file
+            using var data_stream = new StreamWriter("DataFile.txt");
+            foreach (var data_point in lst_astro_data)
+            {
+                data_stream.WriteLine(data_point);
+            }
+            MessageBox.Show("Data was written to file called DataFile.txt in current program folder.");
+        }
+
+        private void lbl_Search_dataList_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_Write_File_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Search_dataFile_Click(object sender, EventArgs e)
+        {
+            // Convert the search string to integer
+            int search_int;
+            if (int.TryParse(txt_Search_dataList.Text, out search_int))
+            {
+                if (lst_astro_data.Contains(search_int)) // the user input an integer and it is found in the dataList
+                {
+                    MessageBox.Show("Found \'" + search_int + "\' at index number " + lst_astro_data.IndexOf(search_int) + " of dataList.");
+                }
+                else // the user input an integer, but it's not found in the dataList
+                { 
+                    MessageBox.Show("\'" + search_int + "\' was not found in data."); 
+                }
+            }
+            else // the user did not input an integer
+            {
+                MessageBox.Show("\'" + txt_Search_dataList.Text + "\' not found, data contains only integers.");
+            }
+
+        }
+
+        private void btn_show_data_Click(object sender, EventArgs e)
+        {
+            // create a string of all the unsorted list elements separated by a comma and space
+            var unsorted_list = string.Join(", ", lst_astro_data);
+            // display the string in a messagebox
+            MessageBox.Show(unsorted_list);
         }
     }
 }
